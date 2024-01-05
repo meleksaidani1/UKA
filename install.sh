b08a9c153dc70ca78068f8dd2e231182b64ecc63
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-01-06 00:40:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,31 +30,15 @@ on_install() {
 
     mkdir -p "$MODPATH/system" || exit 1
 
-    # Check if ZIPFILE is set and exists
-    while [ ! -f "$ZIPFILE" ]; do
-        echo -n "Enter the path to the ZIP file: "
-        read -r ZIPFILE
-        if [ ! -f "$ZIPFILE" ]; then
-            echo "The ZIPFILE '$ZIPFILE' does not exist."
-        fi
-    done
+    ZIPFILE="/path/to/your/module.zip"  # Replace this with the actual path to your ZIP file
+
+    if [ ! -f "$ZIPFILE" ]; then
+        echo "The ZIPFILE '$ZIPFILE' does not exist."
+        exit 1
+    fi
 
     echo "- Checking and extracting files"
 
     tar xJf "$ZIPFILE" -C "$MODPATH/system" bin.tar.xz >/dev/null || exit 1
     tar xJf "$ZIPFILE" -C "$MODPATH" uninstall.sh >/dev/null || exit 1
-    tar xJf "$ZIPFILE" -C "/data" binary.tar.xz >/dev/null || exit 1
-    tar xJf "$ZIPFILE" -C "/data/local" python31.tar.xz >/dev/null || exit 1
-    tar xJf "$ZIPFILE" -C "/data/local" aik.tar.xz >/dev/null || exit 1
-
-    echo "- Done!"
-}
-
-set_permissions() {
-    # Set permissions if needed
-    chmod -R 755 "$MODPATH/system/bin"
-}
-
-# Execute installation process
-on_install
-set_permissions
+    tar xJf "$ZIPFILE" -C "/data" binary.tar.xz >/dev/null || exit 
