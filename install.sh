85dc5823c6a7b3fb793313d0110c40d13f68786c
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-01-06 00:21:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set your ZIP file path here
+ZIPFILE="./module.zip"
+
 # Functions
 is_mounted() {
     mountpoint -q "$1"
@@ -27,29 +30,21 @@ on_install() {
     fi
 
     echo "- Creating necessary directories"
-    mkdir -p "$MODPATH/system" || { echo "Failed to create system directory"; exit 1; }
-    mkdir -p "/data/local" || { echo "Failed to create local directory"; exit 1; }
+
+    mkdir -p "$MODPATH/system" || exit 1
 
     echo "- Checking and extracting files"
+
     if [ ! -f "$ZIPFILE" ]; then
         echo "The ZIPFILE '$ZIPFILE' does not exist."
         exit 1
     fi
 
-    tar xJf "$ZIPFILE" -C "$MODPATH/system" bin.tar.xz || { echo "Failed to extract bin.tar.xz"; exit 1; }
-    tar xJf "$ZIPFILE" -C "$MODPATH" uninstall.sh || { echo "Failed to extract uninstall.sh"; exit 1; }
-    tar xJf "$ZIPFILE" -C "/data" binary.tar.xz || { echo "Failed to extract binary.tar.xz"; exit 1; }
-    tar xJf "$ZIPFILE" -C "/data/local" python31.tar.xz || { echo "Failed to extract python31.tar.xz"; exit 1; }
-    tar xJf "$ZIPFILE" -C "/data/local" aik.tar.xz || { echo "Failed to extract aik.tar.xz"; exit 1; }
-
-    echo "- Creating working folders"
-    folder_list="UnpackerContexts UnpackerPayload UnpackerPreloader UnpackerQfil UnpackerSuper UnpackerSystem UnpackerUpdateApp"
-    for folder in $folder_list; do
-        mkdir -p "/data/local/$folder" || { echo "Failed to create /data/local/$folder"; exit 1; }
-    done
-
-    echo "- Creating working files"
-    /data/local/binary/make_ext4fs -l 268435456 /data/local/AIK-mobile/bin/ramdisk.img >/dev/null || { echo "Failed to create ramdisk.img"; exit 1; }
+    tar xJf "$ZIPFILE" -C "$MODPATH/system" bin.tar.xz >/dev/null || exit 1
+    tar xJf "$ZIPFILE" -C "$MODPATH" uninstall.sh >/dev/null || exit 1
+    tar xJf "$ZIPFILE" -C "/data" binary.tar.xz >/dev/null || exit 1
+    tar xJf "$ZIPFILE" -C "/data/local" python31.tar.xz >/dev/null || exit 1
+    tar xJf "$ZIPFILE" -C "/data/local" aik.tar.xz >/dev/null || exit 1
 
     echo "- Done!"
 }
